540bb629dcc1dee765cdefd45d13e815e9d29ec3
charles.benitez <charles.benitez@mykukun.com>
2026-09-02 18:48:43 -0300

    chore: bootstrap Spring Boot 3.5 project (Java 21)

---
+++ src/main/java/com/mindata/hotelavailability/HotelAvailabilityApplication.java
@@ -0,0 +1,13 @@
+package com.mindata.hotelavailability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAvailabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelAvailabilityApplication.class, args);
+    }
+
+}
